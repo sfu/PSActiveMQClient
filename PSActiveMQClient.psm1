6e21b1feeccc9fbbf4b3ff10c509eb0268ca9a40
Steve Hillman <hillman@sfu.ca>
2017-05-02 21:13:26 -0700

    Start converting to proper PS module format

---
+++ PSActiveMQClient.psm1
@@ -0,0 +1,27 @@
+#Import the .NET DLLs required to talk to ActiveMQ
+
+    Add-Type -Path $PSScriptRoot\lib\Apache.NMS.dll
+
+#Get public and private function definition files.
+    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+    $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+    Foreach($import in @($Public + $Private))
+    {
+        Try
+        {
+            . $import.fullname
+        }
+        Catch
+        {
+            Write-Error -Message "Failed to import function $($import.fullname): $_"
+        }
+    }
+
+# Here I might...
+    # Read in or create an initial config file and variable
+    # Export Public functions ($Public.BaseName) for WIP modules
+    # Set variables visible to the module and its functions only
+
+Export-ModuleMember -Function $Public.Basename
